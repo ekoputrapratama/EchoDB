afd204047b61535ce576f69202f07f62f44b4069
Eko Putra Pratama <ekoputrapratama27@yahoo.com>
2025-10-08 22:15:38 +0800

    add Promise<NetworkProvider> to return type

--- src/network/index.ts
+++ src/network/index.ts
@@ -1,6 +1,8 @@
-export async function loadNetworkProvider() {
+import { NetworkProvider } from '../types';
+
+export async function loadNetworkProvider(): Promise<NetworkProvider> {
   let module;
-  if(typeof window !== "undefined") {
+  if (typeof window !== 'undefined') {
     module = await import('./browser');
   } else {
     module = await import('./node');
